b06d328d9efc50189783b6f63ff9ab24be53ecb6
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-04 16:24:27 +0500

    Array Problem 08

---
+++ 8/app.js
@@ -0,0 +1,8 @@
+var array = [34, 0, 232, 45, 4 - 5, -100, 45];
+var array2 = [];
+for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+    array2[index] = element;
+}
+console.log(array);
+console.log(array2);
---
+++ 8/app.ts
@@ -0,0 +1,10 @@
+let array:number[] = [34,0,232,45,4-5,-100,45]
+let array2:number[] = []
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    array2[index] = element
+    
+}
+console.log(array);
+console.log(array2);
+
