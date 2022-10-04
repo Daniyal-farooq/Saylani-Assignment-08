28792d8345ea5814663f8e219e0cfeef855d6231
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-04 12:23:12 +0500

    Array Problem 05

---
+++ 5/app.js
@@ -0,0 +1,3 @@
+var array = [-10, 0, 10, 11, 45];
+array = array.sort(function (a, b) { return a - b; });
+console.log(array[array.length - 2]);
---
+++ 5/app.ts
@@ -0,0 +1,3 @@
+let array:number[] = [-10,0,10,11,45]
+array = array.sort((a,b) => a-b)
+console.log(array[array.length-2]);
