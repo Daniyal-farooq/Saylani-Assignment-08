145a7cfd534d84d1047f839e8d919e42834750ee
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-06 14:26:29 +0500

    Array Problem 15

---
+++ 15/app.js
@@ -0,0 +1,4 @@
+var array1 = [10, 20, 30];
+var array2 = [40, 50];
+var array3 = array1.concat(array2);
+console.log(array3);
---
+++ 15/app.ts
@@ -0,0 +1,5 @@
+let array1:number[] = [10,20,30]
+let array2:number[] = [40,50]
+let array3:number[] = array1.concat(array2)
+console.log(array3);
+
