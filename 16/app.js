32ca683d43e0904af257997c6b7d1e325fa2b00b
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-06 14:30:03 +0500

    Array Problem 16

---
+++ 16/app.js
@@ -0,0 +1,3 @@
+var array1 = ["This", "is", "a", "string", "array"];
+var array2 = array1.reverse();
+console.log(array2);
---
+++ 16/app.ts
@@ -0,0 +1,3 @@
+let array1:string[] = ["This","is","a","string","array"]
+let array2:string[] = array1.reverse()
+console.log(array2);
