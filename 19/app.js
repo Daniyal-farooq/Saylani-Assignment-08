a232498c88ae82b21e2dd6560fef6fbae597d28d
Daniyal-farooq <mdanifarooq@gmail.com>
2022-10-08 14:03:35 +0500

    Array Problem 19

---
+++ 19/app.js
@@ -0,0 +1,3 @@
+var array = [100, 34, 45, 0, 10, -10];
+var v = array.sort(function (a, b) { return b - a; });
+console.log(v);
---
+++ 19/app.ts
@@ -0,0 +1,4 @@
+let array:number[] = [100,34,45,0,10,-10]
+let v = array.sort((a,b)=> b-a)
+console.log(v);
+
